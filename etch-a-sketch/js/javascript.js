55e4f08bd60a6d7365f6d823ce92edcad6b13fe0
Ian Bayne <ianbayne@gmail.com>
2015-10-25 18:50:17 +0900

    Addition to etch-a-sketch

--- etch-a-sketch/js/javascript.js
+++ etch-a-sketch/js/javascript.js
@@ -1,6 +1,32 @@
 $(document).ready(function(){
-	var height = $('input[name=height]').val();
-	var width = $('input[name=width]').val();	
-	
-	
+    
+    var number = prompt("Enter a number, bitch.");
+    if(number > 18){
+        number = prompt('Eighteen or less, motherfucker.')
+    }
+    
+    $('.reset').on('click', function(){
+        $('body').empty; 
+        number = prompt("Enter a number, bitch.");
+    });
+    
+    var code = '<div class="table">';
+
+    for (i=0; i<number; i++){
+        code+= '<div class="row">';
+            for (j=0; j<number; j++){
+                code+= '<div class="cell"></div>';
+            }
+        code+='</div>';
+    }
+
+    code+= "</div>"
+
+    $('#container').append(code);
+    
+    $('.cell').mouseenter(function(){
+        $(this).addClass('black').fadeIn('slow');
+    });
+        
+
 })
