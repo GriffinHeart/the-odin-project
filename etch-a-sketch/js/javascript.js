1c5c848faf9fa965f4fcaa15a18ba4c1085d4fff
Ian Bayne <ianbayne@gmail.com>
2015-10-26 23:46:15 +0900

    Updated etch-a-sketch

--- etch-a-sketch/js/javascript.js
+++ etch-a-sketch/js/javascript.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
     
     var number = prompt("Enter a number, bitch.");
-    if(number > 18){
-        number = prompt('Eighteen or less, motherfucker.')
-    }
     
     $('.reset').on('click', function(){
-        $('body').empty; 
+        $('#container').empty(); 
         number = prompt("Enter a number, bitch.");
+        
+        var code = '<div class="table">';
+
+        for (i=0; i<number; i++){
+            code+= '<div class="row">';
+            for (j=0; j<number; j++){
+                code+= '<div class="cell"></div>';
+            }
+            code+='</div>';
+        }
+
+        code+= "</div>"
+
+        $('#container').append(code);
+    
+        $('.cell').mouseenter(function(){
+            $(this).addClass('black').fadeIn('slow');
+        });
     });
     
     var code = '<div class="table">';
