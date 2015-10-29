38424c4163d6ea0aa866d2c9e3dfb45ee91ffa78
Ian Bayne <ianbayne@gmail.com>
2015-10-29 23:17:55 +0900

    Added functions

--- etch-a-sketch/js/javascript.js
+++ etch-a-sketch/js/javascript.js
@@ -1,42 +1,14 @@
 $(document).ready(function(){
     
-    var number = prompt("Enter a number, bitch.");
-
-    while(isNaN(number)){
-        alert("That's not a number, dumbshit.");
-        number = prompt("Enter a number, bitch.");
-    } 
+    var number;                                             /* Declare the variable */
     
-    var code = '<div class="table">';
-    for (i=0; i<number; i++){
-        code+= '<div class="row">';
-            for (j=0; j<number; j++){
-                code+= '<div class="cell"></div>';
-            }
-        code+='</div>';
-    }
-
-    code+= "</div>"
-        
-    $('#container').append(code);
-    
-    $('.row div').mouseenter(function(){
-        if($(this).hasClass('black')){    
-        }
-        else
-        {
-            $(this).hide().addClass('black').fadeIn('fast');
-        }
-    });
-    
-    $('.reset').on('click', function(){
-        $('#container').empty(); 
-       
-        number = prompt("Enter a number, bitch.");
+    function getNumberMakeTable(a_number){                  /* Create a function that asks for a number, */
+        number = prompt("Enter a number, bitch.");          /* uses that number for the rows and columns */
+                                                            /* in a table, and then give the table life */
         while(isNaN(number)){
             alert("That's not a number, dumbshit.");
             number = prompt("Enter a number, bitch.");
-        }
+        } 
         
         var code = '<div class="table">';
         for (i=0; i<number; i++){
@@ -44,22 +16,31 @@ $(document).ready(function(){
             for (j=0; j<number; j++){
                 code+= '<div class="cell"></div>';
             }
-            code+='</div>';
-        }   
+        code+='</div>';
+        }
 
         code+= "</div>"
-
+        
         $('#container').append(code);
     
         $('.row div').mouseenter(function(){
-            if($(this).hasClass('black')){
-                
+            if($(this).hasClass('black')){    
             }
             else
             {
                 $(this).hide().addClass('black').fadeIn('fast');
             }
         });
+    }
+    
+    getNumberMakeTable(number);                 /* Run the function */
+    
+    $('.reset').on('click', function(){         /* Make the reset button work */
+        $('#container').empty(); 
+        
+        getNumberMakeTable(number);
         
     }); 
 }); 
+
+
