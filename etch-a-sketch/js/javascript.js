da640179c9b2cc991c66159496602d240d420657
Ian Bayne <ianbayne@gmail.com>
2015-10-29 23:49:51 +0900

    Added commenting

--- etch-a-sketch/js/javascript.js
+++ etch-a-sketch/js/javascript.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var number;                                             /* Declare the variable */
     
     function getNumberMakeTable(a_number){                  /* Create a function that asks for a number, */
+        
         number = prompt("Enter a number, bitch.");          /* uses that number for the rows and columns */
                                                             /* in a table, and then give the table life */
         while(isNaN(number)||number==""){                   /* If the prompted number of rows and columns */
@@ -10,26 +11,25 @@ $(document).ready(function(){
             number = prompt("Enter a number, bitch.");      /* a number. */
         } 
         
-        var code = '<div class="table">';
+        var code = '<div class="table">';                   /* Make that table */
         for (i=0; i<number; i++){
             code+= '<div class="row">';
             for (j=0; j<number; j++){
                 code+= '<div class="cell"></div>';
             }
-        code+='</div>';
+            code+='</div>';
         }
-
         code+= "</div>"
+        $('#container').append(code);                       /* Add the code for the table to the element with */
+                                                            /* id container */    
         
-        $('#container').append(code);
-    
-        $('.row div').mouseenter(function(){
-            if($(this).hasClass('black')){    
-            }
+        $('.row div').mouseenter(function(){                /* Change cell colors when the cursor enters them */
+            if($(this).hasClass('black')){                  /* Check if the cell is already black */
+            }                                               /* If it is, do nothing */
             else
             {
-                $(this).hide().addClass('black').fadeIn('fast');
-            }
+                $(this).hide().addClass('black').fadeIn('fast');    /* If it isn't, hide the white cell and */
+            }                                                       /* fade in the black one */
         });
     }
     
