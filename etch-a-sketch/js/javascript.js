b5576fbbc3752a7746472321fb97d7abaf02cb18
Ian Bayne <ianbayne@gmail.com>
2015-10-29 23:05:34 +0900

    More changes to etch-a-sketch

--- etch-a-sketch/js/javascript.js
+++ etch-a-sketch/js/javascript.js
@@ -7,27 +7,27 @@ $(document).ready(function(){
         number = prompt("Enter a number, bitch.");
     } 
     
-        var code = '<div class="table">';
-        for (i=0; i<number; i++){
-            code+= '<div class="row">';
-                for (j=0; j<number; j++){
-                    code+= '<div class="cell"></div>';
-                }
-            code+='</div>';
-        }
+    var code = '<div class="table">';
+    for (i=0; i<number; i++){
+        code+= '<div class="row">';
+            for (j=0; j<number; j++){
+                code+= '<div class="cell"></div>';
+            }
+        code+='</div>';
+    }
 
-        code+= "</div>"
+    code+= "</div>"
         
-        $('#container').append(code);
-        $('.row div').mouseenter(function(){
-            if($(this).hasClass('black')){
-                
-            }
-            else
-            {
-                $(this).hide().addClass('black').fadeIn('fast');
-            }
-        });
+    $('#container').append(code);
+    
+    $('.row div').mouseenter(function(){
+        if($(this).hasClass('black')){    
+        }
+        else
+        {
+            $(this).hide().addClass('black').fadeIn('fast');
+        }
+    });
     
     $('.reset').on('click', function(){
         $('#container').empty(); 
