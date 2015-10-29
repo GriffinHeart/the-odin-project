66c75f2a25b2d847fc4d09145b880746008be2bb
Ian Bayne <ianbayne@gmail.com>
2015-10-29 23:02:11 +0900

    Even more changes to etch-a-sketch

--- etch-a-sketch/js/javascript.js
+++ etch-a-sketch/js/javascript.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
         number = prompt("Enter a number, bitch.");
     } 
     
-    
         var code = '<div class="table">';
         for (i=0; i<number; i++){
             code+= '<div class="row">';
@@ -30,40 +29,37 @@ $(document).ready(function(){
             }
         });
     
-    
-    
-    
-    
     $('.reset').on('click', function(){
         $('#container').empty(); 
+       
         number = prompt("Enter a number, bitch.");
         while(isNaN(number)){
             alert("That's not a number, dumbshit.");
             number = prompt("Enter a number, bitch.");
         }
         
-            var code = '<div class="table">';
-            for (i=0; i<number; i++){
-                code+= '<div class="row">';
-                for (j=0; j<number; j++){
-                    code+= '<div class="cell"></div>';
-                }
+        var code = '<div class="table">';
+        for (i=0; i<number; i++){
+            code+= '<div class="row">';
+            for (j=0; j<number; j++){
+                code+= '<div class="cell"></div>';
+            }
             code+='</div>';
-            }   
+        }   
 
-            code+= "</div>"
+        code+= "</div>"
 
-            $('#container').append(code);
+        $('#container').append(code);
     
-            $('.row div').mouseenter(function(){
-                if($(this).hasClass('black')){
+        $('.row div').mouseenter(function(){
+            if($(this).hasClass('black')){
                 
-                }
-                else
-                {
-                    $(this).hide().addClass('black').fadeIn('fast');
-                }
-            });
+            }
+            else
+            {
+                $(this).hide().addClass('black').fadeIn('fast');
+            }
+        });
         
     }); 
 }); 
